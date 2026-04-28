94475b4657fea89231a5e021fb538d8c2ce96258
“WapaTzy” <wafarozanu22@gmail.com>
2026-04-28 12:41:06 +0700

    merubah curretn menjadi current

--- DoubleLinkedList.cpp
+++ DoubleLinkedList.cpp
@@ -81,4 +81,15 @@ class DoubleLinkedList
          current->next = newNode; //step 9d: current.next = newNode
 
     }
-  
+    void hapus()
+    {
+        if(START == NULL)
+        {
+            cout << "\nList is empty"<< endl;
+            return;
+        }
+        cout << "\nMasukan nim yg akan didelete";
+        int rollNo;
+        cin >> rollNo;
+
+       
